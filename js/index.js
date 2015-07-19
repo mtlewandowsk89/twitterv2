70dc81470c7bde2e17bf634e47543bb2704d10e2
Matt Lewandowski <mtlewandowsk@gmail.com>
2015-07-19 15:54:45 -0700

    final changes

--- js/index.js
+++ js/index.js
@@ -16,10 +16,10 @@ $(function () {
     var tweetsUrl = "http://localhost:3000/tweets/";
     var usersUrl = "http://localhost:3000/users/";
 
-    
+    getInfo();
 
     $('#main').on('click', 'form', function () {
-    	$(this).addClass('expand')
+    	$(this).addClass('expand');
     });
 
     $('#tweets').on('click', '.thread > .tweet', function() {
@@ -35,24 +35,94 @@ $(function () {
         } else {
             var tweetId = $(this).parents('.thread').find('.tweet').attr('id');
             postReply(currentUser, $message, repliesUrl, tweetId);
-        };
+        }
 
         $message = $parent.siblings('textarea').val('');  
         $(this).parents('.compose').removeClass('expand');               
 
         return false;
-
     });
 
-    $('textarea').keypress(function(){
+    $('textarea').keypress(function (){
 
     	if(this.value.length > 140){
         	return false;
    		}
    		
-    	$('.count').html(140 - this.value.length)
+    	$('.count').html(140 - this.value.length);
 	});
 
+   function renderTweet(user, message, id) {
+        var content = template.tweetTmpl({
+                        id: id,
+                        img: user.img,
+                        handle: user.handle,
+                        message: message
+                    });
+
+        return content;
+    }
+
+    function renderCompose() {
+        var content = template.composeTmpl();
+
+        return content;
+    }
+
+
+    function renderThread(user, message, id) {
+        var content = template.threadTmpl({
+                        tweet: renderTweet(user, message, id),
+                        compose: renderCompose()                    
+                    });
+
+        return content;
+    }
+
+    function postReply(user, message, url, tweetId) {
+        $.post(repliesUrl, {
+            userId: user.id,
+            tweetId: tweetId.slice(6),
+            message: message
+        }).done(function (data) {
+            var content = renderTweet(user, data.message, data.id);
+            $('#' + tweetId).siblings('.replies').append(content);
+        });
+    }
+ 
+    function postTweet(user, message, url) {
+        $.post(tweetsUrl, {
+            userId: user.id,
+            message: message
+        }).done(function (data) {
+            var content = renderThread(user, data.message, data.id);
+            $('#tweets').append(content);
+        });
+    }
+  
+    function getInfo() {
+         $.get(tweetsUrl)
+            .done(function (tweets) {
+                tweets.forEach(function (tweet) {
+                    $.get(usersUrl + tweet.userId, function (tweetUser) {
+                            $('#tweets').append(renderThread(tweetUser, tweet.message, tweet.id));                            
+                        });
+               });
+            });
+
+        $.get(usersUrl)
+            .done(function (users) {
+                users.forEach(function (user) {
+                    $.get(usersUrl  +  user.id + '/replies')
+                        .done(function (replies) {
+                            replies.forEach(function (reply) {
+                                var find = '#tweets #tweet-' + reply.tweetId;
+                                $(find).siblings('.replies')
+                                    .append(renderTweet(user, reply.message, reply.id));
+                            });
+                        });
+                });
+            });
+    }
 
 });
-    
